a4d25a34c10526e0890d1e357835b366968f5551
ryan <ryan-polley@uiowa.edu>
2017-04-24 19:40:07 -0500

    added scroll effect

--- scroll.asm
+++ scroll.asm
@@ -1,6 +1,18 @@
 .data
-	DISPLAY .word 0x1000000
+	DISPLAY: .word 0x1000000
 .text
 
+#temp1 = arr[i]
+#temp2 = arr[i+1]
+#arr[i+1] = temp1
+#temp1 = temp2
+
+
+lw $t1, DISPLAY($t0)
 mainloop:
-	sw
+	addiu $t0, $t0, 4
+	andi $t0, $t0, 	127
+	lw $t2, DISPLAY($t0)
+	sw $t1, DISPLAY($t0)
+	move $t1, $t2
+	beq $0, $0, mainloop
