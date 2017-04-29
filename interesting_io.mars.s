dbcf22487612d282f0ffa3d2bbce8c6de3026791
Drew Kabala <dkabala.2011@gmail.com>
2017-04-28 20:04:34 -0500

    Added io mars files

---
+++ interesting_io.mars.s
@@ -0,0 +1,17 @@
+addu  $s0, $zero, 0x00000048	#H
+addu $s0, $zero, 0x00000065	#e
+addu $s0, $zero, 0x0000006c	#l
+addu $s0, $zero, 0x0000006c	#l
+addu $s0, $zero, 0x0000006f	#o
+addu $s0, $zero, 0x00000020	#
+addu $s0, $zero, 0x00000057	#W
+addu $s0, $zero, 0x0000006f	#o
+addu $s0, $zero, 0x00000072	#r
+addu $s0, $zero, 0x0000006c	#l
+addu $s0, $zero, 0x00000064	#d
+addu $s0, $zero, 0x00000021	#!
+
+
+
+
+
